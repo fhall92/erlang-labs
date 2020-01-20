f892045c8ef4b78bbd6301642ab0c288fac31cd0
Fran Hall <fhall92@gmail.com>
2020-01-20 09:55:30 +0000

    initial commit

---
+++ ErlangLabs/nspace.erl
@@ -0,0 +1,24 @@
+-module(nspace).
+
+-export([]).
+
+startns()->
+    register(namespace,spawn(fun()->namespace(top) end)).
+
+namespace(State) ->
+    receive
+        {From,[Head|Tail],MSG} ->
+            %deal with it!
+            NextNSPid=get(Head),
+            NextNSPid!{From,Tail,MSG},
+        namespace(State);
+    {From,[],{regNS,Pid,Name}} ->
+        %Deal with it
+        put(Name,Pid),
+        namespace(State);
+    {From,[],getPid} ->
+        %Self() function returns own Pid
+        From!{getPid,self()},
+        namespace(State)
+
+end.
