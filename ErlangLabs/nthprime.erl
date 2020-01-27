4bf965d6d502d6f1e02f6217ee99ba0a092909b0
Fran Hall <fhall92@gmail.com>
2020-01-27 11:37:23 +0000

    End of class commit

---
+++ ErlangLabs/nthprime.erl
@@ -0,0 +1,18 @@
+%Compile with c(modulename)
+%Run function with modulename:functionName()
+
+-module(nthprime).
+
+-export([isPrime/1, hasNoDivisors/2, getNthPrime/1]).
+
+isPrime(Number) ->
+    
+end.
+
+hasNoDivisors(N, Count) -> 
+
+end.
+
+getNthPrime(Index) ->
+
+end
